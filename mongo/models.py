f97afdce00bd4b0733a8b64075482f9a8166d05c
Ian Weisberger <ian@thegridhost.com>
2013-11-27 00:37:00 -0800

    initial repo creation

---
+++ mongo/models.py
@@ -0,0 +1,19 @@
+from mongoengine import *
+
+connect("LNP_new",host="localhost")
+
+class Tn(Document):
+    TN = fields.IntField(primary_key=True)
+    LRN = fields.IntField()
+    TXN_ID = fields.IntField()
+    SVType = fields.IntField()
+    SPID=fields.StringField()
+    LNPType=fields.StringField()
+    ActivationTimestamp = fields.DateTimeField()
+    
+    
+class TxnId(Document):
+    
+    
+    LAST_TXN_ID = fields.IntField()
+    TXN_TIMESTAM = fields.DateTimeField()
