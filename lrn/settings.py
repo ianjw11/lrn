ac3fd316fe5cd507d4840552333706bbeb256f68
Ian Weisberger <ian@thegridhost.com>
2013-11-27 05:58:01 -0800

    db settings

--- lrn/settings.py
+++ lrn/settings.py
@@ -15,10 +15,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'LNP',                      # Or path to database file if using sqlite3.
+        'USER':'ian',
+        'PASSWORD':'LNP-dev',
+        
+        'HOST':'localhost',
         # The following settings are not used with sqlite3:
-        'USER': 'ianremote',
-        'PASSWORD': 'wKESzPXGyDUvEXNE',
-        'HOST': '199.19.120.51',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'USER': 'ianremote',
+        #'PASSWORD': 'wKESzPXGyDUvEXNE',
+        #'HOST': '199.19.120.51',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
