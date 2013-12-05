431bbfe5934148b1db5befa234f48f3b66aa54b8
Ian Weisberger <ian@thegridhost.com>
2013-12-04 21:20:00 -0800

    changed to bigints

--- sqldb/models.py
+++ sqldb/models.py
@@ -6,22 +6,22 @@ connect("LNP_new",host="localhost")
 
 
 class Tn(models.Model):
-    ID = models.IntegerField(primary_key=True)
-    TN = models.IntegerField()
-    LRN = models.IntegerField()
+    #ID = models.IntegerField(primary_key=True)
+    TN = models.BigIntegerField()
+    LRN = models.BigIntegerField()
     SVType = models.IntegerField()
     SPID=models.CharField(max_length=25)
     LNPType=models.CharField(max_length=25)
     ActivationTS = models.DateField()
     #ActivationTimestamp = models.DateTimeField(db_column="ActivationTS")
-    TxnId = models.IntegerField(db_column="TXN_ID",default=195114) # set default to TX id of BDD dumps
+    TxnId = models.BigIntegerField(db_column="TXN_ID",default=195114) # set default to TX id of BDD dumps
     RegionId = models.CharField(max_length=3,default="ma")
     class Meta:
         db_table = 'SUBSCRIPTIONVERSION'
     
 class LastTxn(models.Model):
-    LAST_TXN_ID = models.IntegerField()
-    TXN_TIMESTAMP = models.DateTimeField()
+    LAST_TXN_ID = models.BigIntegerField()
+    TXN_TIMESTAMP = models.DateField()
     
     
 from django.contrib import admin
