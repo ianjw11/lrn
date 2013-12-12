b1c3ed1bf549f3ab081c73bda3f9562679e2d814
Ian Weisberger <ian@thegridhost.com>
2013-12-12 02:09:15 -0800

    regex for parse

--- sip/302.py
+++ sip/302.py
@@ -7,6 +7,7 @@ from twisted.application import internet, service
 from pprint import pprint
 import txredisapi as redis 
 from twisted.internet import defer
+import re
 myIP = '199.19.120.51'
 # port to bind this redirect server to
 myport = 5060
@@ -16,12 +17,13 @@ UserAgent = "amazingness"
 class SipProxy(sip.Proxy):
 
     def connect(self):
-        self.pool = redis.lazyConnectionPool(host="localhost",port=6379,poolsize=100,reconnect=True)
+        self.pool = redis.lazyConnectionPool(host="localhost",port=6379,poolsize=5,reconnect=True)
         #print self.pool
         #l = yield self.pool.get("14124173907")
         #print l
     def __init__(self):
         self.connect()
+        self.findtn = re.compile('(?<=sip:)(.*?)(?=@)')
         sip.Proxy.__init__(self, host=myIP, port=myport) 
         
     # generate a SIP redirection response to every SIP request and send it to the originator of the request 
@@ -31,11 +33,15 @@ class SipProxy(sip.Proxy):
         if message.method not in  ['INVITE']:return
         #print("to header :")
         #print sip.parseAddress(message.headers)
+        
+        """
         To = message.headers["to"][0] # get "to" line
         at = To.find("@")  # get position of @ sign, or -1 if no @
         start = To.find("sip:") + 4 # position of to header to start parsing at
         if at != -1: TN = To[start:at]
         else: TN = To[start:]
+        """
+        TN = self.findtn(message.headers["to"][0]).group(1) # use regex to pull out number
         #print ("\n TN IS: " + str(TN))
         """ Yielding the redis results should allow the reactor to continue processing other 
         requests while waiting for response
