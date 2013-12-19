cb25137ff5d20397494b12332eabe8b8ab7836b0
Ian Weisberger <ian@thegridhost.com>
2013-12-19 15:01:57 -0800

    settings

--- sqldb/models.py
+++ sqldb/models.py
@@ -6,35 +6,38 @@ class FixedCharField(models.Field):
     def __init__(self, max_length, *args, **kwargs):
         self.max_length = max_length
         super(FixedCharField, self).__init__(max_length=max_length, *args, **kwargs)
-    def db_type(self):
+    def db_type(self,**kwargs):
         return 'char(%s)' % self.max_length
+class TinyIntField(models.Field):
+    def __init__(self, *args, **kwargs):
+        super(TinyIntField, self).__init__( *args, **kwargs)
+    def db_type(self,**kwargs):
+        return 'tinyint(3)' 
 
 
 class Tn(models.Model):
     ID = models.AutoField(primary_key=True)
-    TN = models.BigIntegerField()
+    TN = models.BigIntegerField(unique=True,db_index=True)
     LRN = models.BigIntegerField()
-    SVType = models.PositiveSmallIntegerField(default=0)
-    SPID=FixedCharField(max_length=5)
+    SVType = TinyIntField()
+    SPID=FixedCharField(max_length=4)
     LNPType=FixedCharField(max_length=5)
-    #ActivationTS = models.DateField()
-    ActivationTimestamp = models.DateTimeField(db_column="ActivationTS")
-    TxnId = models.BigIntegerField(db_column="TXN_ID") # set default to TX id of BDD dumps
-    RegionId = FixedCharField(max_length=2)
+    ActivationTimestamp = models.DateTimeField()#db_column="ActivationTS")
+    TxnId = models.BigIntegerField(db_column="TXN_ID",db_index=True) # set default to TX id of BDD dumps
+    RegionId = FixedCharField(max_length=2,default="")
     class Meta:
         db_table = 'SUBSCRIPTIONVERSION'
     
 class Block(models.Model):
     ID = models.AutoField(primary_key=True)
-    NPANXXX = models.BigIntegerField(db_column="NPA_NXX_X")
-    LRN = models.BigIntegerField(null=True)
-    SVType = models.IntegerField(null=True)
-    SPID=models.CharField(max_length=25)
-    #LNPType=models.CharField(max_length=25)
-    ActivationTimestamp = models.DateField(db_column="ActivationTS")
-    #ActivationTimestamp = models.DateTimeField(db_column="ActivationTS")
-    TxnId = models.BigIntegerField(db_column="TXN_ID") # set default to TX id of BDD dumps
-    RegionId = models.CharField(max_length=3,default="")
+    NPANXXX = models.BigIntegerField(db_index=True,unique=True)#,db_column="NPA_NXX_X")
+    LRN = models.BigIntegerField()
+    #SVType = models.PositiveSmallIntegerField(default=0,null=True)
+    SVType = TinyIntField()
+    SPID= FixedCharField(max_length=4)
+    ActivationTimestamp = models.DateField()#db_column="ActivationTS")
+    TxnId = models.BigIntegerField(db_column="TXN_ID",db_index=True) # set default to TX id of BDD dumps
+    RegionId = FixedCharField(max_length=2,default="")
     class Meta:
         db_table = 'NUMBERPOOLBLOCK'
 class LastTxn(models.Model):
